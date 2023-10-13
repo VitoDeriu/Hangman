785b7be160d47970ecafe1677dd8f584e04a30b8
dvito <vito.deriu@ynov.com>
2023-10-13 11:40:35 +0200

    commit post merge

--- fonction/func.go
+++ fonction/func.go
@@ -14,17 +14,17 @@ func ShowTextFromFile(n string) {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	
+
 	sc := bufio.NewScanner(file)
 	lines := make([]string, 0)
 	for sc.Scan() {
 		lines = append(lines, sc.Text())
 	}
-	
+
 	if err := sc.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(lines)
+	fmt.Println(lines[rand])
 }
 func ParsingDico(a string) {
 
