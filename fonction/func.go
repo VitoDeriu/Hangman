412a2b2edbc090b2c156a778de641569a292fa8e
dvito <vito.deriu@ynov.com>
2023-10-13 09:40:50 +0200

    add parsingDico v1

--- fonction/func.go
+++ fonction/func.go
@@ -5,13 +5,29 @@ import (
 	"os"
 )
 
+var List []string
+
 func ShowTextFromFile(n string) {
-	var list []string
+
 	content, err := os.ReadFile(n)
 	if err != nil {
 		fmt.Println("Erreur")
 	} else {
-		list = append(list, string(content))
+		List = append(List, string(content))
 	}
-	fmt.Println(list)
+	fmt.Println(List)
 }
+
+func ParsingDico(a string) {
+
+	for _, dicoParse := range a {
+		var mot []string
+		if dicoParse == '\n' {
+			mot = append(mot, string(dicoParse))
+			fmt.Println(mot)
+		}
+	}
+	return
+}
+
+// on stock le for range dans une variable et quand y'a un \n on met tout ca dans un tableau
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import hangman "Hangman/fonction"
 func main() {
 	name := "noms_monstres.txt"
 	hangman.ShowTextFromFile(name)
+	hangman.ParsingDico(&hangman.List)
 }
